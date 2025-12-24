da4ff8a077e89d62dd89d37e6eb023e83dcd3122
pswon5894 <pswon5894@gmail.com>
2025-12-24 13:02:00 +0900

    현재 위치 웹 지도에서

--- app.js
+++ app.js
@@ -7,10 +7,27 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
+map.locate({setView: true, maxZoom: 16})
+
+map.on('locationfound', function (e) {
+
+  // 현재 위치에 마커 추가
+  L.marker(e.latlng)
+  .addTo(map)
+  .bindPopup('내 현재 위치')
+  .openPopup();
+
+  L.circle(e.latlng, e.accuracy).addTo(map);
+});
+
+map.on('locationerror', function () {
+  alert('위치 정보를 사용할 수 없습니다.');
+});
+
 // 예시 마커 (주차 위치)
-const parkingMarker = L.marker([37.5665, 126.9780])
-    .addTo(map)
-    .bindPopup('현재 주차 위치');
+// const parkingMarker = L.marker([37.5665, 126.9780])
+//     .addTo(map)
+//     .bindPopup('현재 주차 위치');
 
 // 서버에서 위치 받아오는 예시
 /*
